1e952e498f5f3b99773cee4724c5905e4624fc42
LEONAN BAZILIO <leonanbaziliodev@gmail.com>
2025-02-25 21:55:08 -0300

    back adiconado excessoes globais

---
+++ BackStoreManagement/src/main/java/com/storemanagement/StoreManagement/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.storemanagement.StoreManagement.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
---
+++ BackStoreManagement/src/main/java/com/storemanagement/StoreManagement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.storemanagement.StoreManagement.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<String> handleBusinessException(BusinessException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.internalServerError().body("Internal server error: " + ex.getMessage());
+    }
+}
